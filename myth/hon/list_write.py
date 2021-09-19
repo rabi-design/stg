c07117fee5ce74c4a3b064ec735af22ea9c7ffa8
rabi-design <snowsnow123@ezweb.ne.jp>
2021-09-20 01:09:06 +0900

    Changes

--- myth/hon/list_write.py
+++ myth/hon/list_write.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import openpyxl
 
-file="重複チェックファイル.xlsx"
-book1=openpyxl.load_workbook(file)
-sheet1=book1['シート1']
+file = "重複チェックファイル.xlsx"
+book1 = openpyxl.load_workbook(file)
+sheet1 = book1['シート1']
 
-ro=2
-list1=[]
+ro = 2
+list1 = []
 while True:
-    list1.append(sheet1.cell(ro,2).value)
-    ro+=1
-    if sheet1.cell(ro,2).value==None:
-        if sheet1.cell(ro+1,2).value==None:
-            if sheet1.cell(ro+2,2).value==None:
+    list1.append(sheet1.cell(ro, 2).value)
+    ro += 1
+    if sheet1.cell(ro, 2).value is None:
+        if sheet1.cell(ro + 1, 2).value is None:
+            if sheet1.cell(ro + 2, 2).value is None:
                 break
 with open('list_other.py', 'w') as f:
     f.write("urls=[\n")
